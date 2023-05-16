d2726f75ac5775f63b15c92cf212ef8aac32f476
nnickolaev <nickolaev101@yandex.ru>
2023-05-16 16:21:11 +0900

    Убраны комментарии

--- database.py
+++ database.py
@@ -1,90 +1,60 @@
 """Модуль работы с Базой данных
 """
 import sqlalchemy as sq
-from sqlalchemy import create_engine, MetaData, exists, Boolean
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.exc import IntegrityError, InvalidRequestError
-from configg import db_url_object
+from config import db_url_object
 
 
-# Создание таблиц
-metadata = MetaData()  # Функция для управления таблицами
-Base = declarative_base()  # Для того чтобы класс Viewed был унаследован от базовой модели
+metadata = MetaData()
+Base = declarative_base()
 
-# Создание движка алхимии и сессии
-engine = create_engine(db_url_object)  # Создаем движок, для работы с БД
-Session = sessionmaker(bind=engine)  # Используем класс sessionmaker для простоты вызова класса Session
-session = Session()  # Объект session на основе класса Session, созданного sessionmaker'ом для всех взаимодействий с БД
-connection = engine.connect()  # Метод для управления подключением к БД
+engine = create_engine(db_url_object)
+Session = sessionmaker(bind=engine)
+session = Session()
+connection = engine.connect()
 
 
-# Таблица просмотренных анкет
+"""Таблица просмотренных анкет
+"""
+
+
 class Viewed(Base):
-    __tablename__ = 'viewed'  # Название таблицы
+    __tablename__ = 'viewed'
 
-    profile_id = sq.Column(sq.Integer, primary_key=True)  # ID пользователя
-    worksheet_id = sq.Column(sq.Integer, primary_key=True)  # ID просмотренной анкеты
+    profile_id = sq.Column(sq.Integer, primary_key=True)
+    worksheet_id = sq.Column(sq.Integer, primary_key=True)
 
-# Функции для работы с БД
-def create_all():  # Создание таблицы
-    Base.metadata.create_all(engine)  # Используется метод create_all()
+
+def create_all():
+    Base.metadata.create_all(engine)
     print('Создание таблицы')
 
-def wipe_all():  # Очистка таблицы
-    Base.metadata.drop_all(engine)  # Используется метод drop_all()
+
+def wipe_all():
+    Base.metadata.drop_all(engine)
     create_all()
     print('Таблица очищена')
 
-def add_viewed(profile_id, worksheet_id):  # Добавление записи в БД
+
+def add_viewed(profile_id, worksheet_id):
     try:
         to_db = Viewed(profile_id=profile_id, worksheet_id=worksheet_id)
         session.add(to_db)
-        session.commit()  # Коммит для подтверждения транзакции
+        session.commit()
     except (IntegrityError, InvalidRequestError):
         return False
     print('Профиль добавлен в Базу данных')
     return True
 
-def check_viewed(profile_id, worksheet_id):  # Проверка просмотрена ли анкета
+
+def check_viewed(profile_id, worksheet_id):
     from_db = session.query(Viewed).filter(Viewed.profile_id == profile_id).all()
-    worksheets_list = [worksheet.worksheet_id for worksheet in from_db]  # Итерация по просмотренным анкетам, записываем их в список
-    if worksheet_id in worksheets_list:  # Проверяем есть ли новая анкета в списке просмотренных
+    worksheets_list = [worksheet.worksheet_id for worksheet in from_db]
+    if worksheet_id in worksheets_list:
         print('Пользователь уже в Базе данных')
         return True
     else:
         print('Это новый для пользователя профиль')
         return False
-
-
-if __name__ == '__main__':
-    create_all()
-
-    # Проверка создания записи просмотренной анкеты
-    # add_viewed(111, 222)
-
-    # Проверка просмотрена ли анкета
-    # check_viewed(123, 321)
-
-    # Проверка вытаскивания просмотренных в список
-    # q = query_viewed(123)
-    # q
-
-    # Проверка на наличие и занесение в БД
-    # users_record = session.query(Viewed).filter_by(profile_id='123').scalar()
-    # if not users_record:
-    #     users_record = User_search_data(id=item['id'])
-    # session.add(users_record)
-    # session.commit()
-
-    # Тестирую код для проверки просмотра анкеты
-    # worksheet_id = 321
-    # rows = session.query(Viewed).filter(Viewed.profile_id == '123').all()
-    # values = [row.worksheet_id for row in rows]
-    # print(values)
-    # if worksheet_id in values:
-    #     print('true')
-    # else:
-    #     print('false')
-
-    # Проверка функции проверка просмотра анкеты
-    # check_viewed(123, 321)
